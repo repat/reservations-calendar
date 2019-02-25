d5b984e4b4e3a33ff9c1f20e4061ea0d1b71e379
phelm <honeydove_mhar@yahoo.com>
2019-02-26 00:13:17 +0800

    Dont show reservations past calendar date

--- src/Ognjenm/ReservationsCalendar/Gantti.php
+++ src/Ognjenm/ReservationsCalendar/Gantti.php
@@ -230,12 +230,15 @@ class Gantti
      */
     private function createSingleEventHtml($event, $i)
     {
+
         $eventStart = \Carbon\Carbon::createFromTimestamp($event['start']);
+        $eventEnd = \Carbon\Carbon::createFromTimestamp($event['end'])->timestamp;
         $firstEvent = \Carbon\Carbon::createFromTimestamp($this->first->month()->timestamp);
+        $calendarStart = $this->first->month()->past($this->options['monthago'])->timestamp;
 
         if ($eventStart->lessThan($firstEvent)) {
             if($this->options['monthago']) {
-                $diff = $this->first->month()->past($this->options['monthago'])->timestamp;
+                $diff = $calendarStart;
             } else {
                 $diff = $this->first->month()->timestamp;
             }
@@ -270,17 +273,20 @@ class Gantti
             $label_shorten = $event['label'];
         }
 
-        $html = '<span class="gantt-block' . $class . '" style="left: ' . max($left, 0) . 'px; width: ' . $width . 'px; height: ' . $height . 'px; text-align: left;" data-position-left="'.max($left, 0).'" data-width="'.$width.'" data-check-datas="'.$days.' '.$diff.' '.$offset.' '.$event['start'].' '.$event['end'].'">';
+        if($eventEnd >= $calendarStart){
+            $html = '<span data-start="'.$calendarStart.'" data-end="'.$eventEnd.'" class="gantt-block' . $class . ''.($width < 120 ? ' collapsed' : '').'" style="left: ' . max($left, 0) . 'px; width: ' . $width . 'px; height: ' . $height . 'px; text-align: left;" data-position-left="'.max($left, 0).'" data-width="'.$width.'" data-check-datas="'.$days.' '.$diff.' '.$offset.' '.$event['start'].' '.$event['end'].'">';
 
-        if ($event['tooltip']) {
-            $html .= '<a href="' . $url . '" style="margin-top:3px; margin-left:5px;" class="btn black"  data-placement="top" tabindex="' . $i . '" data-html="true" data-trigger="focus" data-toggle="popover" title="' . $label . '" data-content="' . $tooltip . '">' . ($left < 0 ? '...' : '') . ' <i class="fa ' . $icon . '"></i> ' . $tooltip . '</a>';
-        } else {
-            $html .= '<a href="' . $url . '" style="margin-top:3px; margin-left:5px;" class="btn black">' . ($left < 0 ? '...' : '') . ' <i class="fa ' . $icon . '"></i> ' . $tooltip . '</a>';
+            if ($event['tooltip']) {
+                $html .= '<a href="' . $url . '" style="margin-top:3px; margin-left:5px;" class="btn black"  data-placement="top" tabindex="' . $i . '" data-html="true" data-trigger="focus" data-toggle="popover" title="' . $label . '" data-content="' . $tooltip . '">' . ($left < 0 ? '...' : '') . ' <span><i class="fa ' . $icon . '"></i>' . $tooltip . '</span></a>';
+            } else {
+                $html .= '<a href="' . $url . '" style="margin-top:3px; margin-left:5px;" class="btn black">' . ($left < 0 ? '...' : '') . ' <span><i class="fa ' . $icon . '"></i> ' . $tooltip . '</span></a>';
+            }
+
+            $html .= '<span style="display: inline-block; margin-top:3px; margin-left:5px; color:white;">' . $label_shorten . '</span>';
+            $html .= '</span>';
+
+            return $html;
         }
-
-        $html .= '<span style="display: inline-block; margin-top:3px; margin-left:5px; color:white;">' . $label_shorten . '</span>';
-        $html .= '</span>';
-        return $html;
     }
 
     private function createDaysHtml($wrapstyle, $cellstyle)
