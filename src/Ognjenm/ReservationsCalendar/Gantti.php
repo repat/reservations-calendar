0f467b8db584e65ba33da46029493ff81e0d08dc
phelm <honeydove_mhar@yahoo.com>
2019-03-01 15:42:31 +0800

    Hotfix

--- src/Ognjenm/ReservationsCalendar/Gantti.php
+++ src/Ognjenm/ReservationsCalendar/Gantti.php
@@ -236,14 +236,8 @@ class Gantti
         $calendarStart = $this->first->month()->past($this->options['monthago'])->timestamp;
         $calendarStartDate = \Carbon\Carbon::createFromTimestamp($calendarStart);
 
-        if ($eventStart->lessThan($firstEvent)) {
-
-            $diff = $this->first->month()->timestamp;
-
-            if($eventStart->lessThan($calendarStartDate)){
-                $diff = $calendarStart;
-            }
-
+        if($eventStart->lessThan($calendarStartDate)){
+            $diff = $calendarStart;
         } else {
             $diff = $event['start'];
         }
@@ -276,12 +270,12 @@ class Gantti
         }
 
         if($eventEnd >= $calendarStart){
-            $html = '<span class="gantt-block' . $class . ''.($width < 120 ? ' collapsed' : '').'" style="left: ' . max($left, 0) . 'px; width: ' . $width . 'px; height: ' . $height . 'px; text-align: left;" data-position-left="'.max($left, 0).'" data-width="'.$width.'">';
+            $html = '<span data-event-start="'.$eventStart.'" data-first-event="'.$firstEvent.'" class="gantt-block' . $class . ''.($width < 120 ? ' collapsed' : '').'" style="left: ' . max($left, 0) . 'px; width: ' . $width . 'px; height: ' . $height . 'px; text-align: left;" data-position-left="'.max($left, 0).'" data-width="'.$width.'">';
 
             if ($event['tooltip']) {
-                $html .= '<a href="' . $url . '" style="margin-top:3px; margin-left:5px;" class="btn black"  data-placement="top" tabindex="' . $i . '" data-html="true" data-trigger="focus" data-toggle="popover" title="' . $label . '" data-content="' . $tooltip . '">' . ($left < 0 ? '...' : '') . ' <span><i class="fa ' . $icon . '"></i>' . $tooltip . '</span></a>';
+                $html .= '<a href="' . $url . '" style="margin-top:3px; margin-left:5px; overflow:hidden; width: ' . ($width < 120 ? $width - 10: '') . 'px;" class="btn black"  data-placement="top" tabindex="' . $i . '" data-html="true" data-trigger="focus" data-toggle="popover" title="' . $label . '" data-content="' . $tooltip . '">' . ($left < 0 ? '<span class="ellipsis">...</span>' : '') . ' <span class="name"><i class="fa ' . $icon . '"></i> ' . $tooltip . '</span></a>';
             } else {
-                $html .= '<a href="' . $url . '" style="margin-top:3px; margin-left:5px;" class="btn black">' . ($left < 0 ? '...' : '') . ' <span><i class="fa ' . $icon . '"></i> ' . $tooltip . '</span></a>';
+                $html .= '<a href="' . $url . '" style="margin-top:3px; margin-left:5px; overflow:hidden; width: ' . ($width < 120 ? $width - 10: '') . 'px;" class="btn black">' . ($left < 0 ? '<span class="ellipsis">...</span>' : '') . ' <span class="name"><i class="fa ' . $icon . '"></i> ' . $tooltip . '</span></a>';
             }
 
             $html .= '<span style="display: inline-block; margin-top:3px; margin-left:5px; color:white;">' . $label_shorten . '</span>';
